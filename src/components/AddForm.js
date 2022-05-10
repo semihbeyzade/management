e4a3fc06c7c33d3f33ac3edaca05857505a3f6ac
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-05-11 00:53:25 +0200

    installed react-bootstrap

---
+++ src/components/AddForm.js
@@ -0,0 +1,51 @@
+import {Form, Button, FormGroup} from 'react-bootstrap'
+
+const AddForm = () => {
+
+
+    return(
+       <Form>
+
+        <FormGroup>
+            <Form.Control 
+            type='text' 
+            placeholder='Name *' 
+            required 
+            />
+
+            
+        </FormGroup>
+
+        <FormGroup>
+            <Form.Control 
+            type='email' 
+            placeholder='Email *' 
+            required 
+            />
+        </FormGroup>
+
+        <FormGroup>
+        <Form.Control 
+            as='textarea' 
+            placeholder='Address *' 
+            rows={3}
+            />
+        </FormGroup>
+
+        <FormGroup>
+        <Form.Control 
+            type='text' 
+            placeholder='Phone' 
+             
+            />
+        </FormGroup>
+
+        <Button variant="success" type="submit" block>
+            Add New Employee
+        </Button>
+
+       </Form>
+    )
+}
+
+export default AddForm;
--- src/contexts/EmployeeContext.js
+++ src/contexts/EmployeeContext.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const EmployeeContext = createContext();
 
-const EmployeeContextProvider = (props) =>{
+const EmployeeContextProvider = (props) => {
 
     const [employees] = useState([
         {id:uuidv4(), name: 'Semih Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'},
