bc3109098e1322075e26d8b9c62837d6ac4c45a1
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-05-10 22:48:43 +0200

    Created components and used useState

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import Employee from './Employee'
+import React from "react";
+import Employee from "./Employee";
+import { useState } from "react";
 
 const EmployeeList = () => {
+
+ const [employees, setEmployees] = useState([
+      {id:1, name: 'Semih Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'},
+      {id:1, name: 'Sevde Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'},
+      {id:1, name: 'Misha Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'},
+      {id:1, name: 'Pera Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'},
+      {id:1, name: 'Deniz Örscelik', email: 'semih@mail.com', address: 'Mozart str.', phone: '(0171)3598741'}
+  ])
+
   return (
     <div>
-        <Employee />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Employee employees={employees} />
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
