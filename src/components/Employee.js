35e4eddf4977569879e21d7b2c9ba095d694e7d2
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-05-10 22:08:17 +0200

    created set-up

---
+++ src/components/Employee.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div>
+        <ul>
+            <li>Employee 1</li>
+            <li>Employee 2</li>
+            <li>Employee 3</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Employee
